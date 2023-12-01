d2055c3c52920b834d4356ee617209ae5c25709a
Hein I <not@provid.ed>
2023-12-01 14:14:38 +0100

    Lösung für Day01.Part02 hinzugefügt

--- Day01/CalibrationReader.cs
+++ Day01/CalibrationReader.cs
@@ -8,6 +8,17 @@ public class CalibrationReader
         return inputs.Select(ParseCalibrationValue).Sum();
     }
 
+    internal static int SumStringCalibrationValues(string[] lines)
+    {
+        return lines.Select(ParseStringCalibrationValues).Sum();
+    }
+
+    public static int ParseStringCalibrationValues(string input)
+    {
+        string normalizedInput = ReplaceStringNumbers(input);
+        return ParseCalibrationValue(normalizedInput);
+    }
+
     private static int ParseCalibrationValue(string input)
     {
         /// The ascii number values of the characters 0 - 9 are 48 - 57
@@ -18,4 +29,50 @@ public class CalibrationReader
         var numbersInLine = input.Where(IsCharNumber).Select(RemoveAsciiOffset).ToArray();
         return numbersInLine.First() * 10 + numbersInLine.Last();
     }
+
+    private static string ReplaceStringNumbers(string input)
+    {
+        if (input.Length == 0) return string.Empty;
+        if (input.Length == 1) return input;
+
+        if (input.StartsWith("one"))
+        {
+            return "1" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("two"))
+        {
+            return "2" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("three"))
+        {
+            return "3" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("four"))
+        {
+            return "4" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("five"))
+        {
+            return "5" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("six"))
+        {
+            return "6" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("seven"))
+        {
+            return "7" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("eight"))
+        {
+            return "8" + ReplaceStringNumbers(input[1..]);
+        }
+        if (input.StartsWith("nine"))
+        {
+            return "9" + ReplaceStringNumbers(input[1..]);
+        }
+
+        return input[0] + ReplaceStringNumbers(input[1..]);
+    }
+
 }
--- Day01/CalibrationReaderTest.cs
+++ Day01/CalibrationReaderTest.cs
@@ -22,4 +22,42 @@ public class CalibrationReaderTest
         var input = File.ReadAllLines(@"inputs\day01.txt");
         CalibrationReader.SumCalibrationValues(input).Should().Be(55971);
     }
+
+    [Fact]
+    public void SumPart02SampleDatatShouldReturn()
+    {
+        var lines = new[]
+        {
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+        };
+        CalibrationReader.SumStringCalibrationValues(lines).Should().Be(281);
+    }
+    [Fact]
+    public void SumPart02PuzzleInputShouldReturn55971()
+    {
+        var input = File.ReadAllLines(@"inputs\day01.txt");
+        CalibrationReader.SumStringCalibrationValues(input).Should().Be(54663);
+
+    }
+    [Theory]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    [InlineData("eighthree", 83)]
+    [InlineData("sevennine", 79)]
+
+    public void GetCalibrationValuesWithStrings(string input, int expected)
+    {
+        CalibrationReader.ParseStringCalibrationValues(input).Should().Be(expected);
+    }
 }
