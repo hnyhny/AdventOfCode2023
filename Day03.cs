30670fd0166fda3c4978d3787d79d0f677d18ded
Hein I <not@provid.ed>
2023-12-03 17:18:50 +0100

    Day03.Part01 aufgeräumt

--- Day03.cs
+++ Day03.cs
@@ -31,30 +31,26 @@ public class Day03Test
         Day03.Part01(input).Should().Be(4361);
     }
 }
+
+internal class NumberAreaBuilde
+{
+
+}
 public class Day03
 {
     public static int Part01(string[] input)
     {
-        IEnumerable<NumberArea> numberAreaLineFunc(string line, int rowIndex) => Regex.Matches(line, @"(\d+)")
-            .Select(m =>
-                new NumberArea
-                (
-                    Value: Convert.ToInt32(m.Value),
-                    Left: Math.Max(0, m.Index - 1),
-                    Right: Math.Min(m.Index + m.Length, input.Length - 1),
-                    Top: Math.Max(0, rowIndex - 1),
-                    Bottom: Math.Min(rowIndex + 1, input.Length - 1)
-                ));
+        var allNumbers = GetAreas(input);
+        var symbolPositions = GetSymbolPositions(input);
 
-        var symbolPositions = input.SelectMany(GetSymbolIndexes).ToArray();
+        bool HasSymbolNeighbor(NumberArea na) => symbolPositions.Any(na.IsCoveringCell);
 
-        var allNumbers = input.SelectMany(numberAreaLineFunc);
-        var validNumbers = allNumbers.Where(na => symbolPositions.Any(cell => na.IsCoveringCell(cell)))
-                                     .ToArray();
-        ;
+        return allNumbers.Where(HasSymbolNeighbor).Sum(na => na.Value);
+    }
 
-        return validNumbers.Sum(na => na.Value);
-
+    private static Cell[] GetSymbolPositions(string[] input)
+    {
+        return input.SelectMany(GetSymbolIndexes).ToArray();
     }
 
     private static bool IsSymbol(char c) => Regex.IsMatch(c.ToString(), @"[^\d.]");
@@ -66,12 +62,27 @@ public class Day03
                 .Select(t => t.i)
                 .Select(columnNumber => new Cell(columnNumber, rowIndex)); ;
     }
+    private static IEnumerable<NumberArea> GetAreas(string[] input)
+    {
+        return input.SelectMany((s, i) => GetArea(s, i, input.Length));
+
+    }
+    private static IEnumerable<NumberArea> GetArea(string input, int rowIndex, int maxRows)
+    {
+        var maxColumns = input.Length;
+        return Regex.Matches(input, @"(\d+)")
+            .Select(m => new NumberArea
+                (
+                    Value: Convert.ToInt32(m.Value),
+                    Left: Math.Max(0, m.Index - 1),
+                    Right: Math.Min(m.Index + m.Length, maxColumns - 1),
+                    Top: Math.Max(0, rowIndex - 1),
+                    Bottom: Math.Min(rowIndex + 1, maxRows - 1)
+                )).ToArray();
+    }
 }
 
 internal record Cell(int X, int Y);
-internal record SymbolPosition(int Row, int Column)
-{
-}
 
 internal record NumberArea(int Value, int Left, int Right, int Top, int Bottom)
 {
