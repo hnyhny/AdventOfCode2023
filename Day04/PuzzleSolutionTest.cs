1059e7667af82e241b5602a1a3dab5f3fe4d1e8d
Hein I <not@provid.ed>
2023-12-04 18:55:05 +0100

    Day04.Part02 verschönert

--- Day04/PuzzleSolutionTest.cs
+++ Day04/PuzzleSolutionTest.cs
@@ -5,29 +5,18 @@ public class PuzzleSolutionTest
     [Theory]
     [InlineData(@"Day04\sample.part01.txt", 13)]
     [InlineData(@"Day04\puzzle.txt", 26426)]
-
     public void TestPart01(string filepath, int expected)
     {
         var input = File.ReadAllLines(filepath);
-        input.Select(CardParser.ParseCard)
-             .Sum(c => c.Value)
-             .Should().Be(expected);
+        Day04.Part01(input).Should().Be(expected);
     }
+
     [Theory]
     [InlineData(@"Day04\sample.part01.txt", 30)]
-    [InlineData(@"Day04\puzzle.txt", 26426)]
+    [InlineData(@"Day04\puzzle.txt", 6227972)]
     public void TestPart02(string filepath, int expected)
     {
         var input = File.ReadAllLines(filepath);
-        var cards = input.Select(CardParser.ParseCard).ToArray();
-        var cardAmount = cards.ToDictionary(c => c.Id, _ => 1);
-        foreach (var card in cards)
-        {
-            foreach (var id in card.WinsCards)
-            {
-                cardAmount[id] += (cardAmount[card.Id]);
-            }
-        }
-        cardAmount.Values.Sum().Should().Be(expected);
+        Day04.Part02(input).Should().Be(expected);
     }
 }
